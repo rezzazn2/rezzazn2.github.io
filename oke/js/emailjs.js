2429924fd576df21e659e93f8cd78050a8988f75
rezzazn2 <muhammadreza1665@gmail.com>
2024-11-24 17:55:11 +0700

    mantap

---
+++ oke/js/emailjs.js
@@ -0,0 +1,21 @@
+function sendMail(){
+    (function(){
+        emailjs.init("_2bfOzNY3BpkZAVri")
+    })();
+
+    let params = {
+        from_name : document.querySelector("#from_name").value,
+        from_email : document.querySelector("#from_email").value,
+        message : document.querySelector("#message").value
+    };
+
+    let serviceID = "service_va9yysp";
+    let templateID = "template_9322x2d";
+
+    emailjs.send(serviceID,templateID,params)
+    .then( res => {
+        alert("Email berhasil dikirim");
+    }).catch();
+}
+
+console.log( document.querySelector("#message"))
